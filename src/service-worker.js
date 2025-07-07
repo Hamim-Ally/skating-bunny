ab73d98102de7f4869e76d07dba59099e2761f2a
Hamim-Ally <hamimally3105@gmail.com>
2025-07-08 00:13:45 +0600

    Uploading Project

---
+++ src/service-worker.js
@@ -0,0 +1,42 @@
+// service-worker.js
+const CACHE_NAME = 'bunny-game-v1';
+const ASSETS_TO_CACHE = [
+  '/', // root
+  '/index.html',
+  '/bunnyGame.js', // your game logic file if separate
+  'https://assets.codepen.io/264161/rabbit6.glb',
+  'https://esm.sh/three@0.156.1',
+  'https://esm.sh/gsap',
+  // add any shader files or CSS here too
+];
+
+// Install
+self.addEventListener('install', event => {
+  console.log('[SW] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  console.log('[SW] Activate');
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
